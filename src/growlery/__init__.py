c5d13ad5181cb46b73d7859a932133a675672fff
Lari Liuhamo <lari.liuhamo@gmail.com>
2026-05-27 23:08:17 +0300

    feat: Migrate to uv and switch to dynamic versioning

--- src/growlery/__init__.py
+++ src/growlery/__init__.py
@@ -1,1 +1,8 @@
 """Clan Quest OSRS hiscore bot."""
+
+import importlib
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
